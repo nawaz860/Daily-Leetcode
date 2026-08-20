fba68ee10ac93363273255d71b64bd79b064dd5b
Mustehsan Nawaz <mustehsannawaz860@gmail.com>
2026-08-20 14:08:24 +0530

    Time: 1 ms (86.47%), Space: 44.2 MB (57.81%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,27 +1,26 @@
 class Solution {
-    List<List<Integer>> res =new ArrayList<>();
+    List<List<Integer>> res = new ArrayList<>();
 
     public List<List<Integer>> subsets(int[] nums) {
-      findSubsets(nums,0,new ArrayList<>());
+        findSubsets(nums, 0, new ArrayList<>());
         return res;
     }
 
-    public void findSubsets(int nums[], int index, List<Integer> sublist){
+    public void findSubsets(int nums[], int index, List<Integer> sublist) {
         // base case
-        if(index==nums.length){
+        if (index == nums.length) {
             res.add(new ArrayList<>(sublist));
             return;
         }
         //pick
         sublist.add(nums[index]);
-        findSubsets(nums,index+1,sublist);
+        findSubsets(nums, index + 1, sublist);
         //while backtrack we need to remove the last added element
-        sublist.remove(sublist.size()-1);
+        sublist.remove(sublist.size() - 1);
 
         //no pick
-        findSubsets(nums,index+1,sublist);
+        findSubsets(nums, index + 1, sublist);
         //since we dont added any element so, we dont need to remove any element while backtrack
-        
 
     }
 }
