459061556f082b3473c2772dc631d419398d96a2
Mustehsan Nawaz <mustehsannawaz860@gmail.com>
2026-08-20 12:24:54 +0530

    Time: 0 ms (100%), Space: 42.3 MB (17.73%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,12 +1,14 @@
 class Solution {
-        public int climbStairs(int n) {
-                return func(n, 1, 2);
-                    }
+    public int climbStairs(int n) {
+        return func(n, 1, 2);
+    }
 
-                        public int func(int n, int a, int b) {
-                                if (n == 1) return a;
-                                        if (n == 2) return b;
+    public int func(int n, int a, int b) {
+        if (n == 1)
+            return a;
+        if (n == 2)
+            return b;
 
-                                                return func(n - 1, b, a + b);
-                                                    }
-                                                    }
+        return func(n - 1, b, a + b);
+    }
+}
