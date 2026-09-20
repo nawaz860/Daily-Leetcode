45310f61dcb9031a9916887f6b94a4d5c06514d7
Mustehsan Nawaz <mustehsannawaz860@gmail.com>
2026-09-20 21:54:48 +0530

    Time: 1 ms (100%), Space: 44 MB (67.44%) - LeetHub

---
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int reverseDegree(String s) {
+
+        int count=0;
+
+        for(int i=0;i<s.length();i++){
+            count+=(26-(s.charAt(i)-'a'))*(i+1);
+        }
+
+        return count;
+        
+    }
+}
